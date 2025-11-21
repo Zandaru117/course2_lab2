50d8ea867db7e3683323d442a128ab0749f16ec6
Budazhap <zandakovbuda@gmail.com>
2025-11-22 01:26:18 +0700

    commit 5

--- ip_filter.hpp
+++ ip_filter.hpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 
+
 std::vector<std::string> split(const std::string &str, char d);
 void print_ip(auto ip);
 void print_pool(auto ip_pool);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "ip_filter.hpp"
 
+
 int main() {
     uraghaa();
 }
